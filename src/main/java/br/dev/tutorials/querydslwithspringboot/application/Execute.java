68681c7f0492fcd82f4eefe3bab2d0130ee09894
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2020-07-20 12:51:01 -0300

    Melhora nos logs da consulta

--- src/main/java/br/dev/tutorials/querydslwithspringboot/application/Execute.java
+++ src/main/java/br/dev/tutorials/querydslwithspringboot/application/Execute.java
@@ -5,20 +5,18 @@ import br.dev.tutorials.querydslwithspringboot.infrastructure.entity.QEndereco;
 import br.dev.tutorials.querydslwithspringboot.infrastructure.repository.EnderecoRepository;
 import com.google.common.collect.Streams;
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
@@ -32,50 +30,50 @@ public class Execute implements CommandLineRunner {
   public void run(String... args) {
     QEndereco qEndereco = QEndereco.endereco;
 
+    log.info("Com BooleanExpression");
     log.info("Buscando todos os endereços com o id >= 3");
-    BooleanExpression booleanExpression = qEndereco.id.goe(3L);
-    Streams.stream(this.enderecoRepository.findAll(booleanExpression))
+    Streams.stream(this.enderecoRepository.findAll(qEndereco.id.goe(3L)))
         .forEach(t -> log.info("{}", t));
     log.info(" ");
 
+    log.info("Com BooleanBuilder");
     log.info("Buscando todos os endereços com o id >= 2 e bairro terminado com 3");
     long t1 = System.currentTimeMillis();
-    BooleanBuilder builder = new BooleanBuilder();
-    builder.and(qEndereco.id.goe(2L));
-    builder.and(qEndereco.bairro.endsWith("3"));
-    Streams.stream(this.enderecoRepository.findAll(builder)).forEach(t -> log.info("{}", t));
+    Streams.stream(
+            this.enderecoRepository.findAll(
+                new BooleanBuilder().and(qEndereco.id.goe(2L)).and(qEndereco.bairro.endsWith("3"))))
+        .forEach(t -> log.info("{}", t));
     long t2 = System.currentTimeMillis();
-    log.info("Tempo {}", t2 - t1);
+    log.info("Tempo {} ms", t2 - t1);
     log.info(" ");
 
+    log.info("Com BooleanBuilder e JPA API");
     log.info("Buscando todos os endereços com o id >= 2 e bairro terminado com 3");
     t1 = System.currentTimeMillis();
-    BooleanBuilder builder2 = new BooleanBuilder();
-    builder2.and(qEndereco.id.goe(2L));
-    builder2.and(qEndereco.bairro.endsWith("3"));
     new JPAQueryFactory(this.entityManager)
         .selectFrom(qEndereco)
-        .where(builder2)
+        .where(new BooleanBuilder().and(qEndereco.id.goe(2L)).and(qEndereco.bairro.endsWith("3")))
         .fetch()
         .forEach(t -> log.info("{}", t));
     t2 = System.currentTimeMillis();
-    log.info("Tempo {}", t2 - t1);
+    log.info("Tempo {} ms", t2 - t1);
     log.info(" ");
 
+    log.info("Com CriteriaBuilder");
     log.info("Buscando todos os endereços com o id >= 2 e bairro terminado com 3");
     t1 = System.currentTimeMillis();
     CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
-    CriteriaQuery<Endereco> q = criteriaBuilder.createQuery(Endereco.class);
-    Root<Endereco> root = q.from(Endereco.class);
+    CriteriaQuery<Endereco> criteriaQuery = criteriaBuilder.createQuery(Endereco.class);
+    Root<Endereco> root = criteriaQuery.from(Endereco.class);
 
     List<Predicate> predicates = new ArrayList<>();
     predicates.add(criteriaBuilder.and(criteriaBuilder.greaterThanOrEqualTo(root.get("id"), 2)));
     predicates.add(criteriaBuilder.and(criteriaBuilder.like(root.get("bairro"), "%3")));
-    q.where(predicates.toArray(new Predicate[0]));
+    criteriaQuery.where(predicates.toArray(new Predicate[0]));
 
-    this.entityManager.createQuery(q).getResultList().forEach(t -> log.info("{}", t));
+    this.entityManager.createQuery(criteriaQuery).getResultList().forEach(t -> log.info("{}", t));
     t2 = System.currentTimeMillis();
-    log.info("Tempo {}", t2 - t1);
+    log.info("Tempo {} ms", t2 - t1);
     log.info(" ");
 
     System.exit(0);
