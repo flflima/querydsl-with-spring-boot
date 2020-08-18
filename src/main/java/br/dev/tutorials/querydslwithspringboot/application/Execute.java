15f3d3bb2a29c2b573b7a09f651da46618cb06c4
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2020-08-18 14:26:55 -0300

    Adiciona criteria e jpql

--- src/main/java/br/dev/tutorials/querydslwithspringboot/application/Execute.java
+++ src/main/java/br/dev/tutorials/querydslwithspringboot/application/Execute.java
@@ -12,6 +12,14 @@ import br.dev.tutorials.querydslwithspringboot.infrastructure.repository.Telefon
 import com.google.common.collect.Streams;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -25,13 +33,20 @@ public class Execute implements CommandLineRunner {
   private final PessoaRepository pessoaRepository;
   private final EnderecoRepository enderecoRepository;
   private final TelefoneRepository telefoneRepository;
+  private final EntityManager entityManager;
 
   @Override
   public void run(String... args) {
     QPessoa qPessoa = QPessoa.pessoa;
     BooleanExpression booleanExpression = qPessoa.id.eq(1L);
     Iterable<Pessoa> pessoas = this.pessoaRepository.findAll(booleanExpression);
-    Streams.stream(pessoas).forEach(p -> log.info("{}", p));
+    Streams.stream(pessoas).forEach(System.out::println);
+
+    BooleanBuilder builder = new BooleanBuilder();
+    builder.and(qPessoa.id.goe(2L));
+    builder.and(qPessoa.nome.endsWithIgnoreCase("a"));
+    pessoas = this.pessoaRepository.findAll(builder);
+    Streams.stream(pessoas).forEach(System.out::println);
 
     QEndereco qEndereco = QEndereco.endereco;
     BooleanExpression booleanExpression1 = qEndereco.pessoa.id.eq(2L);
@@ -43,10 +58,24 @@ public class Execute implements CommandLineRunner {
     Iterable<Telefone> telefones = this.telefoneRepository.findAll(booleanExpression2);
     Streams.stream(telefones).forEach(t -> log.info("{}", t));
 
-    BooleanBuilder builder = new BooleanBuilder();
     builder.and(qTelefone.pessoa.id.goe(2L));
     builder.and(qTelefone.ddd.endsWith("3"));
     Streams.stream(this.telefoneRepository.findAll(builder)).forEach(t -> log.info("{}", t));
+
+    Query query = this.entityManager.createQuery("Select e from Endereco e where e.bairro like '%a%'");
+    List<Endereco> outrosEnderecos = (List<Endereco>) query.getResultList();
+    outrosEnderecos.forEach(System.out::println);
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Endereco> criteriaQuery = criteriaBuilder.createQuery(Endereco.class);
+    Root<Endereco> root = criteriaQuery.from(Endereco.class);
+
+    List<Predicate> predicates = new ArrayList<>();
+    predicates.add(criteriaBuilder.and(criteriaBuilder.greaterThanOrEqualTo(root.get("id"), 2)));
+    predicates.add(criteriaBuilder.and(criteriaBuilder.like(root.get("bairro"), "%3")));
+    criteriaQuery.where(predicates.toArray(new Predicate[0]));
+
+    this.entityManager.createQuery(criteriaQuery).getResultList().forEach(System.out::println);
 
     System.exit(0);
   }
